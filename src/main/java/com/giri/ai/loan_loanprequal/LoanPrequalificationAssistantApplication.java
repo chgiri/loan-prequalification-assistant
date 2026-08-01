50f4f5c1288ef5a2b30bfa637d718a4daa2b4dcb
Shekhar Giri <261085in@gmail.com>
2026-08-01 14:44:05 +0530

    initial commit

---
+++ src/main/java/com/giri/ai/loan_loanprequal/LoanPrequalificationAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.giri.ai.loan_loanprequal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanPrequalificationAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanPrequalificationAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giri/ai/loan_loanprequal/LoanPrequalificationAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giri.ai.loan_loanprequal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPrequalificationAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
